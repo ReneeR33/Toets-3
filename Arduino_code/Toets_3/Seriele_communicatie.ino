68b13f0619ea45de93375e53c0394498932a7f7b
Mark Tempelman <43167879+MarkTempelman@users.noreply.github.com>
2018-12-21 13:36:09 +0100

    Seriele communicatie

--- Arduino_code/Toets_3/Seriele_communicatie.ino
+++ Arduino_code/Toets_3/Seriele_communicatie.ino
@@ -1,1 +1,31 @@
+String message = "";
 
+void CheckForCommunication() {
+  Serial.begin(9600);
+  char startChar = "#";
+  char stopChar = "%";
+  int incomingByte = 0;
+  bool startReceived = false;
+  Serial.println("check comms");
+
+  if (Serial.available() > 0) {
+    Serial.println("x Received");
+    incomingByte = Serial.read();
+    char receivedCharacter = (char) incomingByte;
+
+    if (receivedCharacter == "%") {
+      startReceived = false;
+      Serial.println("% received");
+    }
+
+    if (startReceived) {
+      message += receivedCharacter;
+      Serial.println("received");
+    }
+
+    if (receivedCharacter == "#") {
+      startReceived = true;
+      Serial.println("# received");
+    }
+  }
+}
