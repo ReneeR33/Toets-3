f0f375f824ca6b60b3b8cf73237581c17d6759d0
Renée <42935816+ReneeR33@users.noreply.github.com>
2018-12-21 13:25:54 +0100

    Code

--- Arduino_code/Toets_3/Led.ino
+++ Arduino_code/Toets_3/Led.ino
@@ -1,23 +1,28 @@
-int red;
-int green;
-int blue;
-
 #define LEDRoodPin 11
 #define LEDGroenPin 9
 #define LEDBlauwPin 10
 
+void SetPinModeLed(){
+  pinMode (LEDRoodPin, OUTPUT);
+  pinMode (LEDGroenPin, OUTPUT);
+  pinMode (LEDBlauwPin, OUTPUT);
+}
+
 void SetColor(int r, int g, int b) {
-  //analogWrite(LEDRoodPin, r);
-  //analogWrite(LEDGroenPin, g);
-  //analogWrite(LEDBlauwPin, b);
+  analogWrite(LEDRoodPin, r);
+  analogWrite(LEDGroenPin, g);
+  analogWrite(LEDBlauwPin, b);
 }
 
 void ChangeColor(float temperature){
+  temperature.round();
   temperature *= 12,75;
   if(temperature <= 255){
+    SetColor(temperature, 0, 255);
     
   }
   if((temperature > 255) && (temperature <= 510)){
-    
+    temperature -= 255;
+    SetColor(255, 0, temperature);
   }
 }
