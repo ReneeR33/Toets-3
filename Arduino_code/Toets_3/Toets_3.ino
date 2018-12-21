21a94d7ede9628633a8b136ddf9e40a3a84664fd
Mark Tempelman <43167879+MarkTempelman@users.noreply.github.com>
2018-12-21 14:03:10 +0100

    fixed more things

--- Arduino_code/Toets_3/Toets_3.ino
+++ Arduino_code/Toets_3/Toets_3.ino
@@ -2,14 +2,10 @@
 
 void setup() {
   
-  Serial.begin(9600); 
-  Serial.begin("Start ");
   StartMeasuring();
  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("loop");
   CheckForCommunication();
 }
